3a93ee0e71908ae6065a4b4caa4184e07b1c40da
Marcus Sjölin <marcussjolin89@gmail.com>
2024-12-15 11:47:44 +0100

    test without plotly

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Dash()
 
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+#    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+#    dcc.Graph(id='graph-content')
 ]
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+# @callback(
+#     Output('graph-content', 'figure'),
+#     Input('dropdown-selection', 'value')
+# )
+# def update_graph(value):
+#     dff = df[df.country==value]
+#     return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run(debug=True)
